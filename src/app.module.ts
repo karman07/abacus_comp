8cf0fab3794b18adef49efba59f2070f474c32ae
karman07 <84911866+karman07@users.noreply.github.com>
2025-07-29 18:22:14 +0530

    done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, Res } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './users/user.module';
 import { PerformanceModule } from './papers/performance.module';
+import { ResultModule } from './result/result.module';
 
 @Module({
   imports: [
@@ -13,7 +14,8 @@ import { PerformanceModule } from './papers/performance.module';
     MongooseModule.forRoot(process.env.MONGO_URI),
     AuthModule,
     UserModule,
-    PerformanceModule
+    PerformanceModule,
+    ResultModule
   ],
 })
 export class AppModule {}
